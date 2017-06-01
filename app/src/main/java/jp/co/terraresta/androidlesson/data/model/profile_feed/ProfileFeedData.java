81cc213c1defae6675b10cef87a327093f365d75
ooyama <ooyama@terraresta.co.jp>
2017-06-01 19:08:25 +0900

    commit Model update

--- app/src/main/java/jp/co/terraresta/androidlesson/data/model/profile_feed/ProfileFeedData.java
+++ app/src/main/java/jp/co/terraresta/androidlesson/data/model/profile_feed/ProfileFeedData.java
@@ -1,7 +1,6 @@
 package jp.co.terraresta.androidlesson.data.model.profile_feed;
 
 import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
@@ -15,7 +14,6 @@ public class ProfileFeedData extends BaseResultData {
 
     @Expose
     private String lastLoginTime;
-    @SerializedName("items")
     @Expose
     private List<ProfileFeedItem> items = null;
 
